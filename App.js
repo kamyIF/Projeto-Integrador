2aeb023cda9adc365b7ceb57bd9c19321239e9d3
JonatasNP <jonatas.nicolau@academico.ifpb.edu.br>
2024-07-30 10:52:13 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import * as Animatable from "react-native-animatable";
+// import {useNavigation} from "@react-navigation/native";
+import { View,Text,StyleSheet, TextInput,TouchableOpacity, StatusBar } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+export default function Register() {
+    return(
+      <View style={styles.container}>
+        <View>
+          <View style={styles.viewCentral}>
+            <View style={styles.textInputContainer} >
+              <Text style={styles.textInputLabel}>Nome Completo</Text>
+              <TextInput style={styles.textInput} />
+            </View>
+            <View style={styles.textInputContainer} >
+              <Text style={styles.textInputLabel}>Email</Text>
+              <TextInput style={styles.textInput} />
+            </View>
+            <View style={styles.textInputContainer} >
+              <Text style={styles.textInputLabel}>Nome do Usuario</Text>
+              <TextInput style={styles.textInput} />
+            </View>
+            <View style={styles.textInputContainer} >
+              <Text style={styles.textInputLabel}>Nome do Usuario</Text>
+              <TextInput style={styles.textInput} />
+            </View>
+          </View>
+        </View>
       <StatusBar style="auto" />
     </View>
-  );
+    );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +37,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#161413"
   },
+  viewCentral: {
+    height: 400,
+    width: 300,
+    borderWidth: 5,
+    borderColor: "#F2B707",
+    borderRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 20
+  },
+  textInputLabel: {
+    color: "#F2B707"
+  },
+  textInput: {
+    backgroundColor: "#2e2d2d",
+    borderRadius: 30
+  },
+  textInputContainer: {
+    gap: 10,
+    marginBottom: 20
+  }
 });
